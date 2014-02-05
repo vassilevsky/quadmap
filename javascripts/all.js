ec2acf110b47c7f55c4799d8f9c43780fd4dca6c
Ilya Vassilevsky <vassilevsky@gmail.com>
2014-02-05 19:35:46 +0400

    Инициализация трёх популярных карт в центре Ульяновска

---
+++ javascripts/all.js
@@ -0,0 +1,38 @@
+(function() {
+  var lat, lon, map1, map2, map3, zoom;
+
+  lat = 54.32;
+
+  lon = 48.4;
+
+  zoom = 14;
+
+  map1 = null;
+
+  map2 = null;
+
+  map3 = null;
+
+  window.onload = function() {
+    map1 = new L.Map('map1');
+    map1.addLayer(new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© OpenStreetMap contributors'
+    }));
+    return map1.setView([lat, lon], zoom);
+  };
+
+  google.maps.event.addDomListener(window, 'load', function() {
+    return map2 = new google.maps.Map(document.getElementById('map2'), {
+      center: new google.maps.LatLng(lat, lon),
+      zoom: zoom
+    });
+  });
+
+  ymaps.ready(function() {
+    return map3 = new ymaps.Map('map3', {
+      center: [lat, lon],
+      zoom: zoom
+    });
+  });
+
+}).call(this);
