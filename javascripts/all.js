c6512aaa58f08fcef99b865c4e36fd891c5d2f90
Ilya Vassilevsky <vassilevsky@gmail.com>
2014-07-27 03:05:39 +0400

    Extract 2GIS implementation details to a separate class

--- javascripts/all.js
+++ javascripts/all.js
@@ -1,6 +1,95 @@
 (function() {
   var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
 
+  this.DoubleGisMap = (function() {
+    DoubleGisMap.prototype._id = null;
+
+    DoubleGisMap.prototype._map = null;
+
+    DoubleGisMap.prototype._centerChangeHandler = null;
+
+    DoubleGisMap.prototype._zoomChangeHandler = null;
+
+    DoubleGisMap.prototype._centerChangeObserver = null;
+
+    DoubleGisMap.prototype._zoomChangeObserver = null;
+
+    function DoubleGisMap(id, lat, lon, zoom) {
+      this._onZoomChange = __bind(this._onZoomChange, this);
+      this._onCenterChange = __bind(this._onCenterChange, this);
+      this._id = id;
+      this._map = new DG.Map(id);
+      this._map.setCenter(new DG.GeoPoint(lon, lat), zoom);
+    }
+
+    DoubleGisMap.prototype.setCenterChangeHandler = function(fn) {
+      this._centerChangeHandler = fn;
+      return this._activateCenterChangeHandler();
+    };
+
+    DoubleGisMap.prototype.setZoomChangeHandler = function(fn) {
+      this._zoomChangeHandler = fn;
+      return this._activateZoomChangeHandler();
+    };
+
+    DoubleGisMap.prototype.getCenter = function() {
+      var center;
+      center = this._map.getCenter();
+      return [center.lat, center.lon];
+    };
+
+    DoubleGisMap.prototype.getZoom = function() {
+      return this._map.getZoom();
+    };
+
+    DoubleGisMap.prototype.setCenter = function(lat, lon) {
+      this._deactivateCenterChangeHandler();
+      this._map.setCenter(new DG.GeoPoint(lon, lat));
+      return this._activateCenterChangeHandler();
+    };
+
+    DoubleGisMap.prototype.setZoom = function(zoom) {
+      this._deactivateZoomChangeHandler();
+      this._map.setZoom(zoom);
+      return this._activateZoomChangeHandler();
+    };
+
+    DoubleGisMap.prototype._activateCenterChangeHandler = function() {
+      this._centerChangeObserver || (this._centerChangeObserver = this._map.addEventListener(this._id, 'DgDragStop', this._onCenterChange));
+      return this._centerChangeObserver.enable();
+    };
+
+    DoubleGisMap.prototype._deactivateCenterChangeHandler = function() {
+      return this._centerChangeObserver.disable();
+    };
+
+    DoubleGisMap.prototype._activateZoomChangeHandler = function() {
+      this._zoomChangeObserver || (this._zoomChangeObserver = this._map.addEventListener(this._id, 'DgZoomChange', this._onZoomChange));
+      return this._zoomChangeObserver.enable();
+    };
+
+    DoubleGisMap.prototype._deactivateZoomChangeHandler = function() {
+      return this._zoomChangeObserver.disable();
+    };
+
+    DoubleGisMap.prototype._onCenterChange = function() {
+      var lat, lon, _ref;
+      _ref = this.getCenter(), lat = _ref[0], lon = _ref[1];
+      return typeof this._centerChangeHandler === "function" ? this._centerChangeHandler(lat, lon) : void 0;
+    };
+
+    DoubleGisMap.prototype._onZoomChange = function() {
+      return typeof this._zoomChangeHandler === "function" ? this._zoomChangeHandler(this.getZoom()) : void 0;
+    };
+
+    return DoubleGisMap;
+
+  })();
+
+}).call(this);
+(function() {
+  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+
   this.GoogleMaps = (function() {
     GoogleMaps.prototype._map = null;
 
@@ -254,7 +343,7 @@
 
 }).call(this);
 (function() {
-  var d, dgis_zoom_observer, lat, lon, maps, setCenter, setZoom, zoom, _ref, _ref1;
+  var d, lat, lon, maps, setCenter, setZoom, zoom, _ref, _ref1;
 
   d = function() {
     return console.debug(arguments);
@@ -289,9 +378,7 @@
     }
     if (source_map_name !== 'dgis') {
       d("set dgis center to " + lat + ", " + lon);
-      dgis_zoom_observer.disable();
-      maps.dgis.setCenter(new DG.GeoPoint(lon, lat));
-      dgis_zoom_observer.enable();
+      maps.dgis.setCenter(lat, lon);
     }
   };
 
@@ -321,9 +408,7 @@
     if (source_map_name !== 'dgis') {
       if (maps.dgis.getZoom() !== new_zoom) {
         d("set dgis zoom to " + new_zoom);
-        dgis_zoom_observer.disable();
         maps.dgis.setZoom(new_zoom);
-        dgis_zoom_observer.enable();
       }
     }
   };
@@ -370,17 +455,12 @@
     })(this));
   });
 
-  dgis_zoom_observer = null;
-
   DG.autoload(function() {
-    maps.dgis = new DG.Map('map4');
-    maps.dgis.setCenter(new DG.GeoPoint(lon, lat), zoom);
-    maps.dgis.addEventListener('map4', 'DgDragStop', function() {
-      var new_center;
-      new_center = maps.dgis.getCenter();
-      return setCenter(new_center.lat, new_center.lon, 'dgis');
+    maps.dgis = new DoubleGisMap('map4', lat, lon, zoom);
+    maps.dgis.setCenterChangeHandler(function(lat, lon) {
+      return setCenter(lat, lon, 'dgis');
     });
-    return dgis_zoom_observer = maps.dgis.addEventListener('map4', 'DgZoomChange', function() {
+    return maps.dgis.setZoomChangeHandler(function(zoom) {
       return setZoom('dgis');
     });
   });
